2c7ac7f63e1a32d43fa9cf4bfbfec367b364b085
Henrik Enblom <henblom@spotify.com>
2023-03-19 22:07:35 +0100

    Init all hardware

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <Arduino.h>
 #include "HID-Project.h"
+#include "Trill.h"
 
-#define PIN_BUTTON          13
+#define PIN_FIRE            13
+#define PIN_MENU            1
 #define PIN_UP              0
 #define PIN_RIGHT           5
 #define PIN_LEFT            7
@@ -12,25 +14,36 @@ bool lastNorthState = false;
 bool lastEastState = false;
 bool lastWestState = false;
 bool lastSouthState = false;
+bool lastMenuState = false;
+bool leftTouchActive = false;
+bool rightTouchActive = false;
+
+Trill trillSensorLeft;
+Trill trillSensorRight;
 
 void setup() {
-    pinMode(PIN_BUTTON, INPUT);
+    pinMode(PIN_FIRE, INPUT);
+    pinMode(PIN_MENU, INPUT);
     pinMode(PIN_UP, INPUT);
     pinMode(PIN_RIGHT, INPUT);
     pinMode(PIN_LEFT, INPUT);
     pinMode(PIN_DOWN, INPUT);
+    trillSensorLeft.setup(Trill::TRILL_SQUARE);
+    trillSensorRight.setup(Trill::TRILL_SQUARE, 0x29);
+    trillSensorLeft.setNoiseThreshold(80);
+    trillSensorRight.setNoiseThreshold(80);
     BootKeyboard.begin();
     BootKeyboard.releaseAll();
 }
 
 void loop() {
-    bool currentState = digitalRead(PIN_BUTTON);
+    bool currentState = digitalRead(PIN_FIRE);
     if (currentState != lastButtonState) {
-        Serial.println("Button changed");
+        Serial.println("Fire changed");
         if (currentState) {
-            BootKeyboard.press(KEY_F12);
+            BootKeyboard.press(KEYPAD_5);
         } else {
-            BootKeyboard.release(KEY_F12);
+            BootKeyboard.release(KEYPAD_5);
         }
     }
     lastButtonState = currentState;
@@ -74,4 +87,32 @@ void loop() {
         }
     }
     lastSouthState = currentState;
+    currentState = digitalRead(PIN_MENU);
+    if (currentState != lastMenuState) {
+        Serial.println("Menu changed");
+        if (currentState) {
+            BootKeyboard.press(KEY_F12);
+        } else {
+            BootKeyboard.release(KEY_F12);
+        }
+    }
+    lastMenuState = currentState;
+
+    trillSensorLeft.read();
+    if (trillSensorLeft.getNumTouches() > 0 || trillSensorLeft.getNumHorizontalTouches() > 0) {
+        BootKeyboard.press(KEY_SPACE);
+        leftTouchActive = true;
+    } else if (leftTouchActive) {
+        BootKeyboard.release(KEY_SPACE);
+        leftTouchActive = false;
+    }
+
+    trillSensorRight.read();
+    if (trillSensorRight.getNumTouches() > 0 || trillSensorRight.getNumHorizontalTouches() > 0) {
+        BootKeyboard.press(KEY_ESC);
+        rightTouchActive = true;
+    } else if (rightTouchActive) {
+        BootKeyboard.release(KEY_ESC);
+        rightTouchActive = false;
+    }
 }
